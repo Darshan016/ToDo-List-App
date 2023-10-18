6f955234e443a922d8f1be38bbce2c3128d2f747
Darshan Panchal <panchaldarshan121@gmail.com>
2023-10-18 23:30:44 +0530

    added bootstrap classes

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './../App.css'
 
 function TodoList(props) {
   return (
-    <li className='list-item'>
-        {props.item}
-        <span onClick={e=>{
-            props.deleteItem(props.index)
-        }}>-</span>
+    <li className='list-group-item d-flex justify-content-between align-items-center'>
+      <span className="todo-item">{props.item}</span>
+      <div className="btn-group">
+        <button
+          className="btn btn-outline-danger"
+          onClick={() => {
+            props.deleteItem(props.index);
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </li>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
