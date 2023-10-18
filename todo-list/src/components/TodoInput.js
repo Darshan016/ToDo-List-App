6d74eb47bbd0766cb9b18a935501b4902b5a1086
Darshan Panchal <panchaldarshan121@gmail.com>
2023-10-18 23:30:06 +0530

    added the desired bootstrap classes

--- todo-list/src/components/TodoInput.js
+++ todo-list/src/components/TodoInput.js
@@ -1,22 +1,49 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 function TodoInput(props) {
-    const [InputText, setInputText] = useState('');
-    const handleEnterPress=(e)=>{
-        if(e.keyCode===13){
-            props.addList(InputText)
-            setInputText("")
-        }
-    }
-  return (
-    <div className='input-container'>
-        <input type='text' value={InputText} className='input-box-todo' placeholder='add an item' onChange={e=>{setInputText(e.target.value)}} onKeyDown={handleEnterPress}/>
+  const [InputText, setInputText] = useState('');
 
-        <button className='add-btn' onClick={()=>{props.addList(InputText)
-        setInputText("")}}>+</button>
+  const handleEnterPress = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      if (InputText.trim() !== '') {
+        props.addList(InputText);
+        setInputText('');
+      }
+    }
+  }
+
+  const handleAddClick = (e) => {
+    e.preventDefault();
+    if (InputText.trim() !== '') {
+      props.addList(InputText);
+      setInputText('');
+    }
+  }
+
+  return (
+    <div className="input-container text-center">
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          value={InputText}
+          className="form-control input-box-todo"
+          placeholder="Add an item"
+          onChange={e => setInputText(e.target.value)}
+          onKeyDown={handleEnterPress}
+        />
+        <div className="input-group-append">
+          <button
+            className="btn btn-outline-success"
+            onClick={handleAddClick}
+            style={{ marginLeft: '10px' }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TodoInput
+export default TodoInput;
