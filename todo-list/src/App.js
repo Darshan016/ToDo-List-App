e582d7edd80d2fb6d37a8fa205f653d49ac6a8f8
Darshan Panchal <panchaldarshan121@gmail.com>
2023-10-18 23:32:01 +0530

    add the bootstrap classes

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,34 +1,39 @@
-import React from 'react'
-import TodoInput from './components/TodoInput'
-import { useState } from 'react'
-import TodoList from './components/TodoList'
+import React from 'react';
+import TodoInput from './components/TodoInput';
+import { useState } from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
-  const [ListTodo,SetListTodo] = useState([])
-  let addList = (InputText)=>{
-    if(InputText!=='')
-      SetListTodo([...ListTodo,InputText])
+  const [ListTodo, SetListTodo] = useState([]);
+
+  let addList = (InputText) => {
+    if (InputText !== '') SetListTodo([...ListTodo, InputText]);
   }
 
-  const deleteListItem=(key)=>{
-    let newItem=[...ListTodo]
-    newItem.splice(key,1)
-    SetListTodo([...newItem])
+  const deleteListItem = (key) => {
+    let newItem = [...ListTodo];
+    newItem.splice(key, 1);
+    SetListTodo([...newItem]);
   }
+
   return (
-   <div className='main-container'>
-    <div className='center-container'>
-      <TodoInput addList={addList} />
-      <h1 className='app-heading'> Your ToDo List</h1>
-      <hr />
-      {ListTodo.map((ListItem,i)=>{
-        return(
-          <TodoList key={i} item={ListItem} deleteItem={deleteListItem} index={i}/>
-        )
-      })}
+    <div className="container border rounded p-4 mt-5 bg-light">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h1 className="mb-4 text-center">Your ToDo List</h1>
+          <TodoInput addList={addList} />
+          <hr />
+          <ul className="list-group">
+            {ListTodo.map((ListItem, i) => {
+              return (
+                <TodoList key={i} item={ListItem} deleteItem={deleteListItem} index={i} />
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
